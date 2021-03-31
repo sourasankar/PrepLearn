c93ae2b0e6f1ce3d997962ea708c1d37a8d0e6d2
sourakootty <soura.kootty4@gmail.com>
2021-03-31 12:31:48 +0530

    Update practice.php

--- practice.php
+++ practice.php
@@ -7,6 +7,17 @@
 
 	$sql = "SELECT * FROM questions WHERE sub_category_id='$sub_category_id'";
 	$result = $conn->query($sql);
+    $count=0;
+    while($row = $result->fetch_assoc()){
+        $questionId[$count]=$row['question_id'];
+        $ans[$count]=$row["answer"];
+        $question[$count]=$row["question"];
+        $option1[$count]=$row["option1"];
+        $option2[$count]=$row["option2"];
+        $option3[$count]=$row["option3"];
+        $option4[$count]=$row["option4"];
+        $count++;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -40,36 +51,34 @@
             <u><?php echo $row2["sub_category_name"]; ?></u>
 		</div>
 
-        <?php 
-            $i=1;
-			while($row = $result->fetch_assoc()){
-                $ans = $row["answer"];
+        <?php             
+			for($j=0;$j<$count;$j++){
 		?>		
         <div class="questionbox">
             <div class="question">
-                Q<?php echo $i; ?>. <?php echo $row["question"]; ?>
+                Q<?php echo $j+1; ?>. <?php echo $question[$j]; ?>
             </div>
             <div style="margin: 15px 40px 0 40px;">
-                <div class="options" onclick='this.style.backgroundColor = "<?php  echo ($ans==1)?("#0bab0bba"):("#fd1f1fab");?>";'> 
-                    <?php echo $row["option1"]; ?>
+                <div id="<?php echo $questionId[$j]."option1"; ?>" class="options" onclick='answer("<?php echo $questionId[$j]."option1"; ?>",<?php echo $questionId[$j]; ?>,1)'> 
+                    <?php echo $option1[$j]; ?>
                 </div>
-                <div class="options" onclick='this.style.backgroundColor = "<?php  echo ($ans==2)?("#0bab0bba"):("#fd1f1fab");?>";'>
-                    <?php echo $row["option2"]; ?>
+                <div id="<?php echo $questionId[$j]."option2"; ?>" class="options" onclick='answer("<?php echo $questionId[$j]."option2"; ?>",<?php echo $questionId[$j]; ?>,2)'>
+                    <?php echo $option2[$j]; ?>
                 </div>
-                <div class="options" onclick='this.style.backgroundColor = "<?php  echo ($ans==3)?("#0bab0bba"):("#fd1f1fab");?>";'>
-                    <?php echo $row["option3"]; ?>
+                <div id="<?php echo $questionId[$j]."option3"; ?>" class="options" onclick='answer("<?php echo $questionId[$j]."option3"; ?>",<?php echo $questionId[$j]; ?>,3)'>
+                    <?php echo $option3[$j]; ?>
                 </div>
-                <div class="options" onclick='this.style.backgroundColor = "<?php  echo ($ans==4)?("#0bab0bba"):("#fd1f1fab");?>";'>
-                    <?php echo $row["option4"]; ?>
+                <div id="<?php echo $questionId[$j]."option4"; ?>" class="options" onclick='answer("<?php echo $questionId[$j]."option4"; ?>",<?php echo $questionId[$j]; ?>,4)'>
+                    <?php echo $option4[$j]; ?>
                 </div>
-                <a href="javascript:void(0)" style="color: #212529;font-weight: bold;font-size: 15px;" onclick="answer(<?php echo $row['question_id']; ?>)">View Answer</a>
+                <a href="javascript:void(0)" style="color: #212529;font-weight: bold;font-size: 15px;" onclick="view_answer(<?php echo $questionId[$j]; ?>)">View Answer</a>
             </div>
-            <div id="<?php echo $row["question_id"]; ?>" style="margin: 10px 0 0 40px;display: none;">                
-                <img src="solutions/<?php echo $row["question_id"]; ?>.png">
+            <div id="<?php echo $questionId[$j]; ?>" style="margin: 10px 0 0 40px;display: none;">                
+                <img src="solutions/<?php echo $questionId[$j]; ?>.png">
             </div>
 		</div>
         <?php 
-                $i++;
+                
             }
             
             //connection to db close
@@ -85,13 +94,41 @@
     <?php  require "php/footer.php"; ?>
 
     <script>
-        function answer(id){
+        var answers = {
+            <?php 
+                for($j=0;$j<$count;$j++){
+                    echo "$questionId[$j]".":"."$ans[$j]";
+                    if($j!=$count-1){
+                        echo ",";
+                    }
+                }    
+            ?>
+        };
+        var flag = {
+            <?php 
+                for($j=0;$j<$count;$j++){
+                    echo "$questionId[$j]".":"."0";
+                    if($j!=$count-1){
+                        echo ",";
+                    }
+                }    
+            ?>
+        };
+        function view_answer(id){
             if(document.getElementById(id).style.display=="none"){
                 document.getElementById(id).style.removeProperty("display");
             }
             else{
                 document.getElementById(id).style.setProperty("display","none");
             }            
+        }
+        function answer(optionid,id,option){
+            //if(answers[id]==option){
+                document.getElementById(optionid).style.setProperty("backgroundColor","#0bab0bba");
+            //}
+            //else{
+              //  document.getElementById(optionid).style.setProperty("backgroundColor","#fd1f1fab");
+           // }
         }
     </script>
     
